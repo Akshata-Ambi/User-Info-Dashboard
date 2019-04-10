082cea618603ebdd8a5bd88047937859b0a78a6e
Akshata Ambi <aksh-ambi@gmail.com>
2019-04-10 14:27:39 +0530

    Adding store and connecting app to store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
-import styles from './App.css';
+import style from './App.css';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
+      <Provider store={store}>
+       <div className={style.app}>
+        <header className={style.appHeader}>
+          <img src={logo} className={style.appLogo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className={styles.Applink}
+            className={style.appLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -21,6 +24,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+      </Provider>
     );
   }
 }
